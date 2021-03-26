de72bb02e4fb38ba04f0b32cce3ee9b11127365e
Emnite2g <78147407+Emnite2g@users.noreply.github.com>
2021-03-26 10:25:59 +0100

    Add files via upload

---
+++ Palindrome.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindromy
+{
+    class Palindrome
+    {
+        public static void PalindromeCheck(string wordToCheck)
+        {
+            wordToCheck = wordToCheck.ToLower();
+            wordToCheck = wordToCheck.Replace(" ", "");
+            string wordReverse = "";
+            for(int i = wordToCheck.Length - 1; i >= 0; i--)
+            {
+                wordReverse = wordReverse + wordToCheck[i];
+            }            
+            Console.WriteLine(wordReverse);
+            if(wordReverse == wordToCheck)
+            {
+                Console.WriteLine("The word is palindrome");
+            }
+            else
+            {
+                Console.WriteLine("The word isn't palindrome");
+            }
+
+        }
+        public static void PalindromeExamples()
+        {
+            Console.WriteLine("Palindromes: ");
+            Console.WriteLine(" redivider, deified, civic, radar, level");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Palindromy
+{
+    class Program
+    {
+        static void Menu()
+        {
+            Console.WriteLine("1.Check if a word is a palindrome");
+            Console.WriteLine("2.Palindrome Examples");
+            Console.WriteLine("3.Exit");
+        }
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Menu();
+                string input = Console.ReadLine();
+                
+                switch (input)
+                {
+                    case "1":
+                        Console.Write("Type word to check: ");
+                        string isPalindrome = Console.ReadLine();
+                        Palindrome.PalindromeCheck(isPalindrome);
+                        break;
+                    case "2":
+                        Palindrome.PalindromeExamples();
+                        break;
+                    case "3":
+                        Environment.Exit(0);
+                        break;
+                }
+                Console.ReadLine();
+                Console.Clear();          
+                        
+            }
+        }
+    }
+}
