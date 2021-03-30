f6ad71e87db95558ee0ee816ebc2809dfb8eb08e
Emnite2g <ksoja@student.agh.edu.pl>
2021-03-30 15:20:28 +0200

    Folder transfer

---
+++ Figures/Mean.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolaFigur
+{
+    public class Mean
+    {
+        
+       
+        public static void ArithmeticMeanSelection()
+        {
+            Console.Write("Enter the count of numbers: ");
+            int CountArithmeticNumbers = 0;            
+            CountArithmeticNumbers = CountOfNumbers();
+            ArithmeticMean(CountArithmeticNumbers);
+            Console.ReadLine();
+        }
+        public static void GeometricMeanSelection()
+        {
+            Console.Write("Enter the count of numbers: ");
+            int CountGeometricNumbers = CountOfNumbers();
+            GeometricMean(CountGeometricNumbers);
+            Console.ReadLine();
+        }
+        
+
+        
+        static int CountOfNumbers()
+        {
+            try
+            {
+                int a = Convert.ToInt32(Console.ReadLine());
+                return a;
+            }
+            catch
+            {
+                Console.WriteLine("Wrong Input!");
+                return CountOfNumbers();
+
+            }
+        }
+        public static void ArithmeticMean(int n)
+        {
+
+            double temp, result;
+            double sum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter a number: " + (i + 1));
+                temp = Functions.DoubleCheck();
+                sum += temp;
+            }
+            result = Math.Round(sum / n, 2);
+            Console.WriteLine("Arithmetic mean: " + result);
+
+        }
+        public static void GeometricMean(int n)
+        {
+            double power = 1.0 / n;
+            double temp, result;
+            double sum = 1;
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Enter a number: " + (i + 1));
+                temp = Functions.DoubleGreater0();
+                sum *= temp;
+            }            
+            result = Math.Pow(sum, power);
+            result = Math.Round(result, 2);
+            Console.WriteLine("Geometric mean: " + result);
+        }
+    }
+}
---
+++ Figures/MenuChoice.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolaFigur
+{
+    public class MenuChoice
+    {
+        public static void AreaMenuChoice()
+        {                     
+                Menu.AreasMenu();
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        TriangleAreaChoice();
+                        break;
+                    case "2":
+                        SquareChoice();
+                        break;
+                    case "3":
+                        TrapezeAreaChoice();
+                        break;
+                    case "4":
+                        Circle.AreaCircleFormula();
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        AreaMenuChoice();
+                        break;
+                }            
+        }
+        public static void PerimeterMenuChoice()
+        {                        
+                Menu.AreasMenu();
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        TrianglePerimeterChoice();
+                        break;
+                    case "2":
+                        Square.SquarePerimeter();
+                        break;
+                    case "3":
+                        TrapezePerimeterChoice();
+                        break;
+                    case "4":
+                        Circle.CirclePerimeter();
+                        break;
+                    case "5":
+                        break;
+                    default:
+                        Console.WriteLine("Wrong Input");
+                        PerimeterMenuChoice();
+                        break;                
+            }
+        }
+        public static void TriangleAreaChoice()
+        {
+            Menu.TriangleAreaMenu();
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Triangle.TriangleAreaFormula1();
+                    break;
+                case "2":
+                    Triangle.TriangleAreaFormula2();
+                    break;
+                case "3":
+                    Triangle.TriangleAreaFormula3();
+                    break;
+                case "4":
+                    Triangle.TriangleAreaFormula4();
+                    break;
+                case "5":
+                    Triangle.TriangleAreaFormula5();
+                    break;
+                case "6":
+                    Triangle.TriangleAreaFormula6();
+                    break;
+                case "7":
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    TriangleAreaChoice();
+                    break;
+            }
+        }         
+       
+        public static void TrianglePerimeterChoice()
+        {
+            Menu.TrianglePerimeterMenu();
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Triangle.TrianglePerimeter1();
+                    break;
+                case "2":
+                    Triangle.TrianglePerimeter2();
+                    break;
+                case "3":
+                    Triangle.TrainglePerimeter3();
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input!");
+                    TrianglePerimeterChoice();
+                    break;
+            }
+        }
+        public static void SquareChoice()
+        {
+            Menu.SquareAreaMenu();
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Square.SquareAreaFormula1();
+                    break;
+                case "2":
+                    Square.SquareAreaFormula2();
+                    break;
+                case "3":
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    SquareChoice();
+                    break;
+            }
+        }
+        public static void TrapezeAreaChoice()
+        {
+            Menu.TrapezeAreaMenu();
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Trapeze.TrapezAreaFormula1();
+                    break;
+                case "2":
+                    Trapeze.TrapezAreaFormula2();
+                    break;
+                case "3":
+                    Trapeze.TrapezAreaFormula3();
+                    break;
+                case "4":
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input!");
+                    TrapezeAreaChoice();
+                    break;
+            }
+        }
+        public static void TrapezePerimeterChoice()
+        {
+            Menu.TrapezePerimeterMenu();
+            string input = Console.ReadLine();
+            switch (input)
+            {
+                case "1":
+                    Trapeze.TrapezePerimeter1();
+                    break;
+                case "2":
+                    Trapeze.TrapezePerimeter2();
+                    break;
+                case "3":
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    TrapezePerimeterChoice();
+                    break;
+            }
+        }
+        public static void MeanChoice()
+        {
+            Console.WriteLine("Arithmetic mean(a), Geometric(g),Back (b)");
+            string input = Console.ReadLine();
+
+            input = input.ToLower().Trim();
+            switch (input)
+            {
+                case "a":
+                    Mean.ArithmeticMeanSelection();
+                    break;
+                case "g":
+                    Mean.GeometricMeanSelection();
+                    break;
+                case "b":
+                    break;
+                default:
+                    Console.WriteLine("Wrong Input");
+                    MeanChoice();
+                    break;
+            }
+        }
+
+    }
+}
