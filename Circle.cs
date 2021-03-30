7ca5fb527a6f53a1708dd62e1483f64a9392ba5e
Krzysztof <78147407+Emnite2g@users.noreply.github.com>
2021-03-30 15:18:19 +0200

    Figures and averages
    Application for calculating area, perimeter of figures and arithmetic and geometric mean.

---
+++ Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolaFigur
+{
+    public class Circle
+    {
+        public static void CirclePerimeter()
+        {
+            Console.WriteLine("2PIr");
+            Console.Write("r: ");
+            double r = Functions.DoubleGreater0();
+            double result = 2 * Math.PI * r;
+            result = Math.Round(result, 2);
+            Console.WriteLine("Perimeter of Circle: " + result);
+            Console.ReadLine();
+        }
+        public static void AreaCircleFormula()
+        {
+            Console.WriteLine("PIr^2");
+            Console.Write("r: ");
+            double r = Functions.DoubleGreater0();
+            double result = Math.PI * r * r;
+            result = Math.Round(result, 2);
+            Console.WriteLine("Area of circle: " + result);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Functions.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PolaFigur
+{
+    public class Functions
+    {
+        public static double DoubleCheck()
+        {
+            try
+            {
+                double a = Convert.ToDouble(Console.ReadLine());
+                return a;
+            }
+            catch
+            {
+                Console.WriteLine("Wrong Input!");                
+                return DoubleCheck();
+            }
+        }
+        public static double DoubleGreater0()
+        {
+            double a = DoubleCheck();
+            if(a > 0)
+            {
+                return a;
+            }
+            else
+            {
+                Console.WriteLine("Value lower than or equal to 0!");
+                return DoubleGreater0();
+            }
+        }
+        public static double DoubleCheck2PI()
+        {
+            double a = DoubleGreater0();
+            if(a < Math.PI * 2)
+            {
+                return a;
+            }
+            else
+            {
+                Console.WriteLine("Value must be lower than 2 PI!");
+                return DoubleCheck2PI();
+            }
+
+        }
+    }
+}
